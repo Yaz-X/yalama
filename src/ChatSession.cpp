5791fe3e4221a9143438451bfe9a9afc4ea57ef2
Yaz <57402639+Yaz-X@users.noreply.github.com>
2026-09-20 15:56:36 +0300

    Update ChatSession.cpp

--- src/ChatSession.cpp
+++ src/ChatSession.cpp
@@ -1,7 +1,7 @@
 /*
     YALAMA Runtime
     Copyright 2026 Yazeed Hamdan
-    Licensed under the Apache License, Version 2.0
+    Licensed under the YALAMA Community License.
     See LICENSE file in project root.
 */
 #include "ChatSession.h"
